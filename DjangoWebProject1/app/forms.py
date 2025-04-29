56e2c627ce0e7dbee556d2da5fa457ee41ce982d
broomthelast <broomthelast@gmail.com>
2025-04-29 06:38:03 +0300

    Update forms.py

--- DjangoWebProject1/app/forms.py
+++ DjangoWebProject1/app/forms.py
@@ -4,7 +4,6 @@ Definition of forms.
 
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
-from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import User
 from django.core.validators import RegexValidator
 from django.core.validators import EmailValidator
